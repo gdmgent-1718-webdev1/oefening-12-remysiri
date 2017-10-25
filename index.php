4815e9a4048df86c6a44447c72a7d7bf29214fbc
remysiri <remysiri@student.arteveldehs.be>
2017-10-25 18:15:40 +0200

    HHH

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+$cities = ['Tokyo', 'Mexico City', 'New York City', 'Mumbai', 'Seoul', 'Shanghai', 'Lagos', 'Buenos Aires', 'Cairo', 'London'];
+
+sort($cities);
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Oefening 12</title>
+</head>
+<body>
+
+
+	<ol>
+<?php foreach ( $cities as $city ): ?>
+		<li><?=strtoupper($city)?></li>
+<?php endforeach; ?>
+	</ol>
+	
+</body>
+</html>
